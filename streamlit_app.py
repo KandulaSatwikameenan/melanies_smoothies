5db420e0059e43d1b6b774a90bcc4ce6703f06bb
Satwika Meenan Kandula <136298090+KandulaSatwikameenan@users.noreply.github.com>
2025-03-16 23:27:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import pandas as pd
 import requests
 from snowflake.snowpark.functions import col
 
@@ -25,7 +26,12 @@ session = cnx.session()  # Use this and remove get_active_session()
 
 # Fetch fruit options
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
+#Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
+pd_df=my_dataframe.topandas()
+st.dataframe(pd_df)
 st.stop()
 
 # Let users choose ingredients
